9ad5d62357bbbd048649c49f5be74da2cf815c30
Rohith-1208 <rohithmurugan12082005@gmail.com>
2026-03-07 19:25:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,10 @@
-from flask import Flask, Response
-import cv2
+from flask import Flask
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)
-
-eye_cascade = cv2.CascadeClassifier(
-    cv2.data.haarcascades + "haarcascade_eye.xml"
-)
-
-def generate_frames():
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        eyes = eye_cascade.detectMultiScale(gray, 1.3, 5)
-
-        for (x, y, w, h) in eyes:
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-@app.route('/')
-def video():
-    return Response(generate_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route("/")
+def home():
+    return "<h1>Eye Tracking System Running Successfully!</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
